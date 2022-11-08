6ad6f12077152130304aadf13668abfc59e5b363
KALIMI <karenmirakyan@gmail.com>
2022-11-08 22:27:12 +0400

    filter view added

--- DatingChamber/Utils/AppColors.swift
+++ DatingChamber/Utils/AppColors.swift
@@ -14,7 +14,7 @@ struct AppColors {
     static let light_purple = Color(UIColor(red: 247/255, green: 244/255, blue: 252/255, alpha: 1))
 //    static let addProfileImageBG = Color(UIColor(red: 246/255, green: 241/255, blue: 255/255, alpha: 1))
     static let onlineStatus = Color(UIColor(red: 110/255, green: 219/255, blue: 154/255, alpha: 1))
-//    static let filterGray = Color(UIColor(red: 87/255, green: 87/255, blue: 87/255, alpha: 1))
+    static let filterGray = Color(UIColor(red: 87/255, green: 87/255, blue: 87/255, alpha: 1))
 //    static let starColor = Color(UIColor(red: 110/255, green: 141/255, blue: 219/255, alpha: 1))
 //    static let reportColor = Color(UIColor(red: 235/255, green: 80/255, blue: 80/255, alpha: 1))
 //    static let grayTextColor = Color(UIColor(red: 18/255, green: 18/255, blue: 18/255, alpha: 1))
--- DatingChamber/Views/Swipes/Swipes.swift
+++ DatingChamber/Views/Swipes/Swipes.swift
@@ -9,14 +9,47 @@ import SwiftUI
 
 struct Swipes: View {
     @StateObject var swipesVM = SwipesViewModel()
+    @State private var showFilter: Bool = false
+    @State private var offsetOnDrag: CGFloat = 0
     
     var body: some View {
-        if swipesVM.loading {
-            ProgressView()
-        } else {
+        NavigationStack {
             ZStack {
-                ForEach(swipesVM.users, id: \.id) { user in
-                    SwipeCard(user: user)
+                if swipesVM.loading {
+                    ProgressView()
+                } else {
+                    ZStack {
+                        ForEach(swipesVM.users, id: \.id) { user in
+                            SwipeCard(user: user)
+                        }
+                    }.frame(maxHeight: .infinity, alignment: .top)
+                        .padding(.top, 30)
+                }
+                Filter(present: $showFilter, gender: $swipesVM.gender, status: $swipesVM.status, range: $swipesVM.ageRange)
+                    .offset(y: showFilter ?  -UIScreen.main.bounds.size.height/4: -UIScreen.main.bounds.size.height)
+                    .animation(.interpolatingSpring(mass: 1.0, stiffness: 100.0, damping: 50, initialVelocity: 0), value: showFilter)
+                    .offset(y: offsetOnDrag)
+                    .gesture(DragGesture()
+                        .onChanged({ (value) in
+                            if value.translation.height < 0 {
+                                self.offsetOnDrag = value.translation.height
+                            }
+                        }).onEnded({ (value) in
+                            if value.translation.height < 0 {
+                                self.showFilter = false
+                                self.offsetOnDrag = 0
+                            }
+                        }))
+            }.navigationBarItems(leading: TextHelper(text: "Dating Chamber",
+                                                     fontName: "Inter-Bold",
+                                                     fontSize: 28).kerning(0.56), trailing: Button {
+                showFilter.toggle()
+            } label: {
+                Image("icon_filter")
+                    .foregroundColor(showFilter ? AppColors.primary : .black)
+            }).onChange(of: showFilter) { value in
+                if !value {
+                    //                    reload
                 }
             }
         }
