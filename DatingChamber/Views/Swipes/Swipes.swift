7c386cd19443ebc4711a71cc5dc9fc4582833d9d
KALIMI <karenmirakyan@gmail.com>
2022-11-08 21:27:46 +0400

    swipes hardcoded

--- DatingChamber/Views/Swipes/Swipes.swift
+++ DatingChamber/Views/Swipes/Swipes.swift
@@ -8,8 +8,18 @@
 import SwiftUI
 
 struct Swipes: View {
+    @StateObject var swipesVM = SwipesViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if swipesVM.loading {
+            ProgressView()
+        } else {
+            ZStack {
+                ForEach(swipesVM.users, id: \.id) { user in
+                    SwipeCard(user: user)
+                }
+            }
+        }
     }
 }
 
