cb6a5b0278b6b5a767aeed2f5a6e4e193d19ef42
KALIMI <karenmirakyan@gmail.com>
2022-11-10 17:02:57 +0400

    query by interests

--- DatingChamber/Services/SwipesService.swift
+++ DatingChamber/Services/SwipesService.swift
@@ -40,10 +40,14 @@ extension SwipesService: SwipesServiceProtocol {
             let ref = db.collection("Users")
             var query: Query = ref
             
+            let encodedUserInterests = try await ref.document(userID).getDocument().get("interests")
+            let interests = try Firestore.Decoder().decode([String].self, from: encodedUserInterests)
+            
             if gender != NSLocalizedString("all", comment: "") { query = ref.whereField("gender", isEqualTo: gender) }
             if online != NSLocalizedString("all", comment: "") { query = query.whereField("online", isEqualTo: true) }
             if maxAge < 51 { query = query.whereField("birthday", isGreaterThanOrEqualTo: dateMaxLimit) }
             if minAge > 18 { query = query.whereField("birthday", isLessThanOrEqualTo: dateMinLimit) }
+            query = query.whereField("interests", arrayContainsAny: interests)
             
             if lastDocSnapshot == nil   { query = query.limit(to: 10) }
             else                        { query = query.start(afterDocument: lastDocSnapshot!).limit(to: 10) }
@@ -58,3 +62,56 @@ extension SwipesService: SwipesServiceProtocol {
     }
 
 }
+
+
+// can be copied for location queries if future
+// interests field should be added to filter and add indexes
+//do {
+//    let ref = db.collection("Users")
+//
+//    // get my location
+//    let encodedlocation = try await ref.document(userID).getDocument().get("location")
+//    let location = try Firestore.Decoder().decode(LocationModel.self, from: encodedlocation)
+//
+//    // create center and radus of the request
+//    let center = CLLocationCoordinate2D(latitude: location.lat, longitude: location.lng)
+//    let radiusInM: Double = 50 * 1000
+//
+//    let queryBounds = GFUtils.queryBounds(forLocation: center,
+//                                          withRadius: radiusInM)
+//
+//    let queries = queryBounds.map { bound -> Query in
+//
+//        let ref = db.collection("Users")
+//        var query: Query = ref
+//
+//        if gender != NSLocalizedString("all", comment: "") { query = query.whereField("gender", isEqualTo: gender) }
+//        if online != NSLocalizedString("all", comment: "") { query = query.whereField("online", isEqualTo: true) }
+//        if maxAge < 51 { query = query.whereField("birthday", isGreaterThanOrEqualTo: dateMaxLimit) }
+//        if minAge > 18 { query = query.whereField("birthday", isLessThanOrEqualTo: dateMinLimit) }
+//
+//        if lastDocSnapshot == nil   { query = query.limit(to: 2) }
+//        else                        { query = query.start(afterDocument: lastDocSnapshot!).limit(to: 2) }
+//
+//        return query
+//            .order(by: "birthday")
+//            .order(by: "location.hash")
+//            .start(at: [bound.startValue])
+//            .end(at: [bound.endValue])
+//    }
+//
+//    var users = [SwipeModel]()
+//    var lastDoc: QueryDocumentSnapshot?
+//
+//    for query in queries {
+//        let docs = try await query.getDocuments().documents
+//        users.append(contentsOf: try docs.map { try $0.data(as: SwipeModel.self ) } )
+//        print(users)
+//        lastDoc = docs.last
+//    }
+//
+//    return .success((users, lastDoc))
+//
+//} catch {
+//    return .failure(error)
+//}
