816f3eba30a5ad244b64d3ccfc5802cef81bd034
KALIMI <karenmirakyan@gmail.com>
2022-11-21 21:39:41 +0400

    Without security token

---
+++ DatingChamber/Utils/AppPreviewModel.swift
@@ -0,0 +1,12 @@
+//
+//  AppPreviewModel.swift
+//  DatingChamber
+//
+//  Created by Karen Mirakyan on 08.11.22.
+//
+
+import Foundation
+struct AppPreviewModel {
+    static let swipeModel = SwipeUserViewModel(user: SwipeModel(id: "yo1NBb8aLlPbC1wJE5pdeJ4fZC92", avatar: Credentials.img_url, name: "Karen", birthday: .now, online: true, isVerified: false, interests: ["fuck", "you", "smth", "coffee", "tea", "chill", "travel"]), interests: ["tea", "coffee"])
+}
+
