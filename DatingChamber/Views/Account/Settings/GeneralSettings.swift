6f6d70d54dfa202ffcc51e23cd04672c079f6982
KALIMI <karenmirakyan@gmail.com>
2022-11-25 04:10:46 +0400

    general settings view

--- DatingChamber/Views/Account/Settings/GeneralSettings.swift
+++ DatingChamber/Views/Account/Settings/GeneralSettings.swift
@@ -9,7 +9,40 @@ import SwiftUI
 
 struct GeneralSettings: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack( spacing: 20 ) {
+            
+            
+            ActionButtonHelper(label: NSLocalizedString("notifications", comment: "")) {
+
+            }
+            
+            ActionButtonHelper(label: NSLocalizedString("location", comment: "")) {
+
+            }
+            
+            ActionButtonHelper(label: NSLocalizedString("blockedList", comment: "")) {
+
+            }
+            
+            Spacer()
+            
+        }.frame(minWidth: 0,
+                maxWidth: .infinity,
+                minHeight: 0,
+                maxHeight: .infinity,
+                alignment: .leading)
+        .padding(30)
+        .padding(.bottom, UIScreen.main.bounds.size.height * 0.1)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                TextHelper(text: NSLocalizedString("general", comment: ""),
+                           fontName: "Inter-Black",
+                           fontSize: 24)
+                .kerning(0.56)
+                .padding(.bottom, 10)
+                .accessibilityAddTraits(.isHeader)
+            }
+        }.navigationTitle(Text(""))
     }
 }
 
